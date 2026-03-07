5d6abcc655192e38a368c5317836ee24937d2160
AdilQadimaliyev <13153@holbertonstudents.com>
2026-03-07 16:02:02 +0000

    Fix task 8: uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-result = ""
-for ch in str:
-o = ord(ch)
-if 97 <= o <= 122:
-result += chr(o - 32)
-else:
-result += ch
-print("{}".format(result))
+    for ch in str:
+        o = ord(ch)
+        if 97 <= o <= 122:
+            print("{}".format(chr(o - 32)), end="")
+        else:
+            print("{}".format(ch), end="")
+    print("")
